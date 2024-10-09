e91426138c69412a2eb3b2184d434b79bb8e6da5
Gathhaaan <137616916+Gathhaaan@users.noreply.github.com>
2024-10-09 17:02:33 +0700

    Add files via upload

---
+++ Tugas1Gathan.py
@@ -0,0 +1,6 @@
+nilai = int(input('masukkan nilai anda :'))
+
+if nilai % 2:
+    print(f'angka {nilai} adalah ganjil')
+else:
+    print(f'angka {nilai} adalah genap')
---
+++ Tugas2Gathan.py
@@ -0,0 +1,14 @@
+nilai = int(input('masukkan nilai ujian anda (0-100):'))
+
+if 100 >= nilai >=90:
+    print('feedback : sangat baik')
+elif 89 >= nilai >=80:
+    print('feedback : baik')
+elif 79 >= nilai >=70:
+    print('feedback : baik')
+elif 69 >= nilai >=60:
+    print('feedback : kurang')
+elif 60 > nilai:
+    print('feedback : sangat kurang')
+else:
+    print()
